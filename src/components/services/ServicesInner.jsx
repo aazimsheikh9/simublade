a582b41d1244223ee999534bd0d69d6f1a4fcf03
Sarim Hussain <sarimh9@gmail.com>
2023-07-04 00:32:51 +0530

    Created new component

---
+++ src/components/services/ServicesInner.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import W from "../../assets/w.png";
+import Signal from "../../assets/signal.png";
+import Color from "../../assets/color.png";
+
+export default function ServicesInner() {
+  return (
+    <div className="bg-[#E0E0E0] rounded-lg p-8 max-w-sm lg:w-auto">
+      <div className="bg-[#FFFDFA] border-[#E0E0E0] border rounded-md w-16 h-16 mb-5 "></div>
+      <div className="text-2xl font-bold">UI Design</div>
+      <div className="text-sm mt-5 mb-10">
+        Delve into the minds of your audience, gain insight, With UX research,
+        your strategy takes flight.{" "}
+      </div>
+      <div className="flex space-x-3">
+        <img className="w-8 h-8" src={W} alt="" />
+        <img className="w-8 h-8" src={Signal} alt="" />
+        <img className="w-8 h-8" src={Color} alt="" />
+      </div>
+    </div>
+  );
+}
